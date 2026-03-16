1a54d8a930865aa815d1cc018d7f30b10db10f7d
JohnnyDat06 <hotandat4321@gmail.com>
2026-03-16 16:19:43 +0700

    Feat: Add Player Online Test Sync to client

---
+++ Assets/_Project/Scripts/Networking/ClientPlayerMove.cs
@@ -0,0 +1,61 @@
+using System;
+using StarterAssets;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClientPlayerMove : NetworkBehaviour
+{
+    [SerializeField] private PlayerInput _playerInput;
+    [SerializeField] private StarterAssetsInputs _starterAssetsInputs;
+    [SerializeField] private ThirdPersonController _thirdPersonController;
+    //[SerializeField] private Transform _rootCamera;
+
+    private bool _lastJumpState;
+
+    private void Awake()
+    {
+        _playerInput.enabled = false;
+        _starterAssetsInputs.enabled = false;
+        _thirdPersonController.enabled = false;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        _lastJumpState = false;
+        
+        if (IsOwner)
+        {
+            _playerInput.enabled = true;
+            _starterAssetsInputs.enabled = true;
+            _thirdPersonController.enabled = true;
+            //var _cinemachine = FindObjectOfType<Cinemachine.CinemachineVirtualCamera>();
+            //_cinemachine.Follow = transform;
+        }
+
+        // if (IsServer)
+        // {
+        // }
+    }
+
+    // [Rpc(SendTo.Server)]
+    // private void UpdateInputServerRpc(Vector2 move, Vector2 look, bool jump, bool sprint)
+    // {
+    //     // Replicate jump as a one-shot press to avoid repeated jumps while key is held/latches.
+    //     bool jumpPressed = jump && !_lastJumpState;
+    //     _lastJumpState = jump;
+    //
+    //     _starterAssetsInputs.MoveInput(move);   
+    //     _starterAssetsInputs.LookInput(look);
+    //     _starterAssetsInputs.JumpInput(jumpPressed);
+    //     _starterAssetsInputs.SprintInput(sprint);
+    // }
+    //
+    // private void LateUpdate()
+    // {
+    //     if (!IsOwner) return;
+    //     
+    //     UpdateInputServerRpc(_starterAssetsInputs.move, _starterAssetsInputs.look, _starterAssetsInputs.jump, _starterAssetsInputs.sprint);
+    // }
+}
